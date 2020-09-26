1c72d70445b783bc67d70a029c06c3f07b644a19
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-09-26 19:51:05 +0200

    AskAnswerCoaching done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,9 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params[:question] == 'I am going to work'
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
       @answer = 'Great!'
-    elsif params[:question].include?('?')
+    elsif @question.ends_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
